cedd86c504d1d523c4094fb6b21a204767441983
Dongyeon Park <plzfday@gmail.com>
2022-09-01 22:34:16 +0100

    Remove Option enum in 'mod find' & tidy up functions

--- src/edit.rs
+++ src/edit.rs
@@ -1,5 +1,7 @@
 // TODO: 1. normal 2. match case 3. Match Whole Word 4. Regex
 pub mod find {
+    use regex::Regex;
+
     #[derive(Debug)]
     pub struct FindResult {
         line: usize,
@@ -26,7 +28,7 @@ pub mod find {
         table
     }
 
-    fn kmp(haystack: &Vec<char>, pattern: &Vec<char>, table: &Vec<usize>) -> Option<Vec<usize>> {
+    fn kmp(haystack: &Vec<char>, pattern: &Vec<char>, table: &[usize]) -> Vec<usize> {
         let haystack_size = haystack.len();
         let pattern_size = pattern.len();
 
@@ -34,11 +36,11 @@ pub mod find {
 
         let mut j = 0;
 
-        for i in 0..haystack_size {
-            while j > 0 && pattern[j] != haystack[i] {
+        for (i, item) in haystack.iter().enumerate().take(haystack_size) {
+            while j > 0 && pattern[j] != *item {
                 j = table[j - 1];
             }
-            if pattern[j] == haystack[i] {
+            if pattern[j] == *item {
                 if j == pattern_size - 1 {
                     results.push(i + 2 - pattern_size);
                     j = table[j];
@@ -48,91 +50,76 @@ pub mod find {
             }
         }
 
-        if results.is_empty() {
-            None
-        } else {
-            Some(results)
-        }
+        results
     }
 
-    fn find_match_case(src: &Vec<Vec<char>>, pattern: &Vec<char>) -> Option<Vec<FindResult>> {
+    fn find_match_case(src: &Vec<Vec<char>>, pattern: &Vec<char>) -> Vec<FindResult> {
         let table = prefix_suffix_max_len_table(pattern);
 
         let mut matching: Vec<FindResult> = Vec::new();
 
         let mut i = 1;
         for line in src {
-            match kmp(line, pattern, &table) {
-                Some(line_res) => {
-                    for position in line_res {
-                        matching.push(FindResult {
-                            line: i,
-                            start: position,
-                            end: position + pattern.len(),
-                        })
-                    }
-                    i += 1;
+            let line_res = kmp(line, pattern, &table);
+            if !line_res.is_empty() {
+                for position in line_res {
+                    matching.push(FindResult {
+                        line: i,
+                        start: position,
+                        end: position + pattern.len(),
+                    })
                 }
-                None => ()
+                i += 1;
             }
         }
 
-        if matching.is_empty() {
-            None
-        } else {
-            Some(matching)
-        }
+        matching
     }
 
     // Disclaimer: I am not sure about capitalized Cyrill characters
-    pub fn find(src: &str,
-                pattern: &str,
-                is_match_case: bool,
-                is_words: bool,
-                is_regex: bool) -> Option<Vec<FindResult>> {
+    pub fn find(src: &str, pattern: &str, is_match_case: bool, is_words: bool, is_regex: bool) -> Vec<FindResult> {
         if is_regex {
-            None
-        } else {
-            let mut lines = src.to_string();
-            let mut pat = pattern.to_string();
-
-            if !is_match_case {
-                lines = lines.to_ascii_lowercase();
-                pat = pat.to_ascii_lowercase();
-            }
-
-            let lines: Vec<&str> = lines.split('\n').collect();
-            let lines: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect();
-            let pat: Vec<char> = pat.chars().collect();
-
-            let result = find_match_case(&lines, &pat);
-
-            if is_words {
-                let side_check = |x: &FindResult| {
-                    let line_num = x.line - 1;
-
-                    if (x.start >= 2
-                        && (lines[line_num][x.start - 2].is_alphanumeric()
-                        || lines[line_num][x.start - 2] == '_')
-                    ) || (x.end - 1 < lines[line_num].len()
-                        && (lines[line_num][x.end - 1].is_alphanumeric()
-                        || lines[line_num][x.end - 1] == '_')) {
-                        return false;
-                    }
-
-                    return true;
-                };
-
-                match result {
-                    Some(v) => {
-                        let tmp: Option<Vec<FindResult>> = Some(v.into_iter().filter(side_check).collect());
-                        return tmp;
-                    }
-                    None => ()
-                }
-            }
-
-            result
+            use regex::Regex;
+            let re = Regex::new(src).unwrap();
         }
+
+        let mut lines = src.to_string();
+        let mut pat = pattern.to_string();
+
+        if !is_match_case {
+            lines = lines.to_ascii_lowercase();
+            pat = pat.to_ascii_lowercase();
+        }
+
+        let lines = lines.split('\n').collect::<Vec<_>>();
+        let lines = lines.iter()
+            .map(|x| x.chars().collect())
+            .collect::<Vec<_>>();
+        let pat: Vec<char> = pat.chars().collect();
+
+        let result = find_match_case(&lines, &pat);
+
+        if is_words {
+            let side_check = |x: &FindResult| {
+                let line_num = x.line - 1;
+
+                if (x.start >= 2
+                    && (lines[line_num][x.start - 2].is_alphanumeric()
+                    || lines[line_num][x.start - 2] == '_')
+                ) || (x.end - 1 < lines[line_num].len()
+                    && (lines[line_num][x.end - 1].is_alphanumeric()
+                    || lines[line_num][x.end - 1] == '_')) {
+                    return false;
+                }
+
+                true
+            };
+
+            if !result.is_empty() {
+                return result.into_iter().filter(side_check).collect();
+            }
+        }
+
+        result
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,61 +13,45 @@ mod tests {
     #[test]
     fn find_match_case_works() {
         println!("===find_match_case_works===");
-        match find::find("    #[test]\n
+        let v = find::find("    #[test]\n
     fn it_works() {\n
         let result = 2 + 2;\n
         assert_eq!(result, 4);\n
-    }", "result", true, false, false) {
-            Some(res) => {
-                for i in res {
-                    println!("{:?}", i)
-                }
-            }
-            _ => ()
+    }", "result", true, false, false);
+        for i in v {
+            println!("{:?}", i)
         }
     }
 
     #[test]
     fn normal_find_works() {
         println!("===normal_find_works===");
-        match find::find("    #[test]\n
+        let v = find::find("    #[test]\n
     fn it_works() {\n
         let result = 2 + 2;\n
         assert_eq!(result, 4);\n
-    }", "ResUlT", false, false, false) {
-            Some(res) => {
-                for i in res {
-                    println!("{:?}", i)
-                }
-            }
-            _ => ()
+    }", "ResUlT", false, false, false);
+        for i in v {
+            println!("{:?}", i)
         }
     }
 
     #[test]
     fn normal_find_with_korean_works() {
         println!("===normal_find_with_korean_works===");
-        match find::find("안녕하세요.\n\
-    제 이름은 jeff park입니다.", "JefF", false, false, false) {
-            Some(res) => {
-                for i in res {
-                    println!("{:?}", i)
-                }
-            }
-            _ => ()
+        let v = find::find("안녕하세요.\n\
+    제 이름은 jeff park입니다.", "JefF", false, false, false);
+        for i in v {
+            println!("{:?}", i)
         }
     }
 
     #[test]
     fn find_words_works() {
         println!("===find_words_works===");
-        match find::find("find find_find", "find", false, true, false) {
-            Some(res) => {
-                for i in res {
-                    println!("{:?}", i)
-                }
-            }
-            _ => ()
+        let v = find::find("find find_find", "find", false, true, false);
+        for i in v {
+            println!("{:?}", i)
         }
     }
 }
